aabf3af534339dd61afb7e4d4995cc8721163d5a
lucaspuglielli <lucaspuglielli@gmail.com>
2020-09-16 15:13:25 -0300

    Subindo projeto em Vue.js

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Lista de Tarefas desenvolvida a partir de um crash course de Vue no YouTube</p>
+  </div>
+</template>
+
+<style scoped>
+.about{
+  text-align: center;
+}
+</style>
